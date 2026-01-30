58c39f53eb51b9ac1a0bbb6b3ec0bfeb01d98a66
manalblack <103968256+manalblack@users.noreply.github.com>
2026-01-30 05:04:26 +0200

    fixes

--- backend/databaseConnection.js
+++ backend/databaseConnection.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 
 const supabaseUrl = process.env.PROJECT_URL
+
 const supabaseKey = process.env.SECRET_KEY
+
 // update
 export const supabase = createClient(supabaseUrl, supabaseKey)
